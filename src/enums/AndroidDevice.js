f49773d844e72a136d715783b3710bc042ac5dcc
Aaron <AaronWatson2975@gmail.com>
2019-10-16 20:28:41 -0230

    [Package Change] Added Joi types and finished validation of connection

--- src/enums/AndroidDevice.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AndroidDevice;
-(function (AndroidDevice) {
-    AndroidDevice[AndroidDevice["DevBoard"] = 0] = "DevBoard";
-    AndroidDevice[AndroidDevice["Phone"] = 1] = "Phone";
-    AndroidDevice[AndroidDevice["Tablet"] = 2] = "Tablet";
-})(AndroidDevice || (AndroidDevice = {}));
-exports.AndroidDevice = AndroidDevice;
--- src/enums/Connection.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Connection;
-(function (Connection) {
-    Connection[Connection["Network"] = 0] = "Network";
-    Connection[Connection["USB"] = 1] = "USB";
-})(Connection = exports.Connection || (exports.Connection = {}));
